00a788caf523a14298b6e5290bc0b82e4f7cd100
Khlff <n.hlopunov@ya.ru>
2023-05-27 15:11:34 +0500

    Forms and main model created

--- feedbacker/models.py
+++ feedbacker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    comment = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.title
