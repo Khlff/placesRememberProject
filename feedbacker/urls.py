5f1932b4913b9e2636b8fc69da67b7062b6e14a5
Khlff <n.hlopunov@ya.ru>
2023-05-28 12:54:45 +0500

    Fixed urls

--- feedbacker/urls.py
+++ feedbacker/urls.py
@@ -1,11 +1,12 @@
-from allauth.account.views import LoginView, LogoutView
+from allauth.socialaccount.providers.vk import views as vk_views
 from django.urls import path, include
 
 from feedbacker import views
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
+    path('accounts/vk/login/', vk_views.oauth2_login, name='vk_login'),
     path('new_feedback/', views.create_feedback, name='create_feedback'),
     path('home/', views.home, name='home'),
-    path('', views.default, name= 'default')
+    path('', views.default, name='default')
 ]
